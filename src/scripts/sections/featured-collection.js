72db6286f7355144b7c725be2c7b7469984257be
MCKataoka <mario.c.kataoka@gmail.com>
2020-08-12 23:02:29 +0100

    fixing swiper

--- src/scripts/sections/featured-collection.js
+++ src/scripts/sections/featured-collection.js
@@ -6,7 +6,12 @@
  *
  * @namespace featuredCollection
  */
-import {register} from '@shopify/theme-sections';
+import { register } from "@shopify/theme-sections";
+
+// import Swiper JS
+import Swiper from "swiper/bundle";
+// import Swiper styles
+import "swiper/swiper-bundle.css";
 
 /**
  * Featured collection constructor
@@ -14,12 +19,78 @@ import {register} from '@shopify/theme-sections';
  *
  * @param {string} container - selector for the section container DOM element
  */
-register('featured-collection', {
+
+register("featured-collection", {
   init() {
     this.publicMethod();
   },
 
   publicMethod() {
-    window.console.log('Initialising featured collection section');
+    window.console.log("Initialising featured collection section");
+  }
+});
+
+// a function that waits before an element appears before executing a function
+var waitForElement = function(element, cb) {
+  var time = setInterval(function() {
+    if (document.querySelector(element)) {
+      clearInterval(time);
+      cb();
+    }
+  }, 100);
+};
+
+//The new instance of Swiper had a timing issue. The script took too long to load
+// so I start the instance after some time has passed to allow the script to run
+
+//Starting a new instance of Swiper
+var mySwiper = new Swiper(".swiper-container", {
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 1,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
   },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetweenSlides: 22
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetweenSlides: 22
+    },
+    1328: {
+      slidesPerView: 4,
+      spaceBetweenSlides: 22
+    }
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  }
+});
+
+// This function is checking what products are on sale
+//if the product is on sale then it will clean the pricing string to only have the price
+//and it also inserts the 'sale' tag in the top right of the image
+
+waitForElement(".product-card__name-price span", function() {
+  var prices = document.querySelectorAll(".product-card__name-price span");
+  for (var i = 0; i < prices.length; i++) {
+    if (prices[i].innerHTML.indexOf("Sale") > -1) {
+      if (prices[i].innerHTML.indexOf("from") > -1) {
+        prices[i].innerHTML = prices[i].innerHTML.split("from").pop();
+      } else {
+        prices[i].innerHTML = prices[i].innerHTML.split("Sale").pop();
+      }
+    }
+  }
+  var products = document.querySelectorAll(".product-card__sale");
+  for (var i = 0; i < products.length; i++) {
+    products[i]
+      .querySelector("div.sale")
+      .insertAdjacentHTML("afterbegin", '<div class="button__sale">Sale</div>');
+  }
 });
